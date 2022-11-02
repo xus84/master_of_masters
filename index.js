d26b46773e13f13034f0c03f97589c7987f94b21
g_garsan@hotmail.com <g_garsan@hotmail.com>
2022-11-02 13:38:08 +0100

    start command

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ require('dotenv').config()
 console.log(`The server is running on port:` + process.env.PORT)
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.command("go", async (ctx) => {
-    let message = ` Please use /maestro command to talk to the GREAT MASTER`
-     await ctx.reply(message)
-})
+bot.start((ctx)=> ctx.reply('Introduce /maestro para sentir tu ki'))  
 
 bot.command('maestro', async (ctx)=> {
         await ctx.reply('dime kaizeneka')
