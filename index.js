5c8024e1b64fef4a7788202fb8ef8c8b230190a6
xus84 <g_garsan@hotmail.com>
2022-11-02 13:26:05 +0100

    repared

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ require('dotenv').config()
 console.log(`The server is running on port:` + process.env.PORT)
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.on("go", async (ctx, next) => {
+bot.on("go", async (ctx) => {
     let message = ` Please use /maestro command to talk to the GREAT MASTER`
      await ctx.reply(message)
-     return next()
 })
 
 bot.command('maestro', async (ctx)=> {
