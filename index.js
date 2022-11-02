e03a534dc0e6c03c190d94d6d70c8c8db65b9ff6
xus84 <g_garsan@hotmail.com>
2022-11-02 13:24:47 +0100

    using next()

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ require('dotenv').config()
 console.log(`The server is running on port:` + process.env.PORT)
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.command("go", async (ctx) => {
+bot.on("go", async (ctx, next) => {
     let message = ` Please use /maestro command to talk to the GREAT MASTER`
      await ctx.reply(message)
+     return next()
 })
 
 bot.command('maestro', async (ctx)=> {
